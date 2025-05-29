d2f2193443f5d4d1f5116e22cceef7970aa95780
omar <65119846+itsomort@users.noreply.github.com>
2025-05-29 14:17:12 -0400

    refactor level header generation
    Generation of the headers for logging calls for specific severity levels
    used heavily repeated CMake code. This change refactors the calls to
    configure_file into a shorter and easier to understand loop.

--- tools/cmake/config_files.cmake
+++ tools/cmake/config_files.cmake
@@ -2,14 +2,17 @@ configure_file(
   "${PROJECT_SOURCE_DIR}/include/stumpless/config.h.in"
   "${PROJECT_BINARY_DIR}/include/stumpless/config.h"
 )
+
 configure_file(
   "${PROJECT_SOURCE_DIR}/include/private/config.h.in"
   "${PROJECT_BINARY_DIR}/include/private/config.h"
 )
+
 configure_file(
   "${PROJECT_SOURCE_DIR}/include/test/config.hpp.in"
   "${PROJECT_BINARY_DIR}/include/test/config.hpp"
 )
+
 configure_file(
   "${PROJECT_SOURCE_DIR}/tools/portage/libstumpless.ebuild.in"
   "${PROJECT_BINARY_DIR}/tools/portage/libstumpless-${PROJECT_VERSION}.ebuild"
@@ -22,83 +25,13 @@ configure_file(
 )
 
 # generating level header files
-set(LEVEL_NAME_UPCASE "ALERT")
-set(LEVEL_NAME_DOWNCASE "alert")
-set(LEVEL_SHORTNAME "a")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
+set(NAME_DOWNCASE alert crit debug emerg err info notice warning trace)
+set(NAME_SHORT a c d em er i n w t)
 
-set(LEVEL_NAME_UPCASE "CRIT")
-set(LEVEL_NAME_DOWNCASE "crit")
-set(LEVEL_SHORTNAME "c")
-configure_file(
+foreach(LEVEL_NAME_DOWNCASE LEVEL_SHORTNAME IN ZIP_LISTS NAME_DOWNCASE NAME_SHORT)
+  string(TOUPPER "${LEVEL_NAME_DOWNCASE}" LEVEL_NAME_UPCASE)
+  configure_file(
   "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
   "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "DEBUG")
-set(LEVEL_NAME_DOWNCASE "debug")
-set(LEVEL_SHORTNAME "d")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "EMERG")
-set(LEVEL_NAME_DOWNCASE "emerg")
-set(LEVEL_SHORTNAME "em")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "ERR")
-set(LEVEL_NAME_DOWNCASE "err")
-set(LEVEL_SHORTNAME "er")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "INFO")
-set(LEVEL_NAME_DOWNCASE "info")
-set(LEVEL_SHORTNAME "i")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "NOTICE")
-set(LEVEL_NAME_DOWNCASE "notice")
-set(LEVEL_SHORTNAME "n")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "WARNING")
-set(LEVEL_NAME_DOWNCASE "warning")
-set(LEVEL_SHORTNAME "w")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
-
-set(LEVEL_NAME_UPCASE "TRACE")
-set(LEVEL_NAME_DOWNCASE "trace")
-set(LEVEL_SHORTNAME "t")
-configure_file(
-  "${PROJECT_SOURCE_DIR}/include/stumpless/level/level.h.in"
-  "${PROJECT_BINARY_DIR}/include/stumpless/level/${LEVEL_NAME_DOWNCASE}.h"
-  @ONLY
-)
+  )
+endforeach()
